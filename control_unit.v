6c568d7dd076c88f4243696364aae6b76683e838
shanggeeth <shanggeethk@gmail.com>
2021-06-30 00:28:43 +0530

    Bug fixes in Control Unit

--- control_unit.v
+++ control_unit.v
@@ -8,141 +8,141 @@ module control_unit(
 	output reg [16:0] write_enable,
     output reg [4:0] read_enable,
 	output reg [5:0] increment,
-	output reg [2:0] alu ;
+	output reg [2:0] alu
+    );
 
-    localparam
-    FETCH1 = 8'd1, FETCH2 = 8'd2, FETCH3 = 8'd3,
-    NOP = 8'd4,
-    ADD1 = 8'd5, ADD2 = 8'd6,
-    MUL1 = 8'd7, MUL2 = 8'd8,
-    SUB1 = 8'd9, SUB2 = 8'd10,
-    LODAC1 = 8'd11, LODAC2 = 8'd12, LODAC3 = 8'd13, LODAC4 = 8'd14, LODAC5 = 8'15,
-    LDACAR1 = 8'd16, LDACAR2 = 8'd17,
-    STOAC1 = 8'd18, STOAC2 = 8'd19,
-    MVAC = 8'd20,
-    MOVREG = 8'd21,
-    CLAC1 = 8'd22, CLAC2 = 8'd23,
-    JUMP1 = 8'd24, JUMP2 = 8'd25, JUMP3 = 8'd26,
-    JUMPZ = 8'd27,
-    JUMPZN = 8'd28,
-    JMNZ = 8'd29,
-    JMNZN = 8'd30,
-    INCAC = 8'd31,
-    INCR = 8'd32,
-    INCR3 = 8'd33,
-    SFTR1 = 8'd34, SFTR2 = 8'd35,
-    SFTL1 = 8'd36, SFTL1 = 8'd37,
-    MOVAR1 = 8'd38,
-    MOVAR2 = 8'd39,
-    ENDOP = 8'd40;
+localparam
+FETCH1 = 6'd1, FETCH2 = 6'd2, FETCH3 = 6'd3,
+NOP = 6'd4,
+ADD1 = 6'd5, ADD2 = 6'd6,
+MUL1 = 6'd7, MUL2 = 6'd8,
+SUB1 = 6'd9, SUB2 = 6'd10,
+LODAC1 = 6'd11, LODAC2 = 6'd12, LODAC3 = 6'd13, LODAC4 = 6'd14, LODAC5 = 6'd15,
+LDACAR1 = 6'd16, LDACAR2 = 6'd17,
+STOAC1 = 6'd18, STOAC2 = 6'd19,
+MVAC = 6'd20,
+MOVREG = 6'd21,
+CLAC1 = 6'd22, CLAC2 = 6'd23,
+JUMP1 = 6'd24, JUMP2 = 6'd25, JUMP3 = 6'd26,
+JUMPZ = 6'd27,
+JUMPZN = 6'd28,
+JMNZ = 6'd29,
+JMNZN = 6'd30,
+INCAC = 6'd31,
+INCR = 6'd32,
+INCR3 = 6'd33,
+SFTR1 = 6'd34, SFTR2 = 6'd35,
+SFTL1 = 6'd36, SFTL2 = 6'd37,
+MOVAR1 = 6'd38,
+MOVAR2 = 6'd39,
+ENDOP = 6'd40;
 
-    localparam
-    NO_W = 17'b0_0000_0000_0000_0000,
-    DATA_MEM_W = 17'b0_0000_0000_0000_0001,
-    ALU_AC_W = 17'b0_0000_0000_0000_0010,
-    IR_PC_W = 17'b0_0000_0000_0000_0100,
-    IR_W = 17'b0_0000_0000_0000_1000,
-    AR_W = 17'b0_0000_0000_0001_0000,
-    X_W = 17'b0_0000_0000_0010_0000,
-    Y_W = 17'b0_0000_0000_0100_0000,
-    Z_W = 17'b0_0000_0000_1000_0000,
-    STXY_W = 17'b0_0000_0001_0000_0000,
-    STYZ_W = 17'b0_0000_0010_0000_0000,
-    STXY_W = 17'b0_0000_0100_0000_0000,
-    R_W = 17'b0_0000_1000_0000_0000,
-    R1_W = 17'b0_0001_0000_0000_0000,
-    R2_W = 17'b0_0010_0000_0000_0000,
-    R3_W = 17'b0_0100_0000_0000_0000,
-    DR_W = 17'b0_1000_0000_0000_0000,
-    AC_W = 17'b1_0000_0000_0000_0000;
+localparam
+NO_W = 17'b0_0000_0000_0000_0000,
+DATA_MEM_W = 17'b0_0000_0000_0000_0001,
+ALU_AC_W = 17'b0_0000_0000_0000_0010,
+IR_PC_W = 17'b0_0000_0000_0000_0100,
+IR_W = 17'b0_0000_0000_0000_1000,
+AR_W = 17'b0_0000_0000_0001_0000,
+X_W = 17'b0_0000_0000_0010_0000,
+Y_W = 17'b0_0000_0000_0100_0000,
+Z_W = 17'b0_0000_0000_1000_0000,
+STXY_W = 17'b0_0000_0001_0000_0000,
+STYZ_W = 17'b0_0000_0010_0000_0000,
+STXZ_W = 17'b0_0000_0100_0000_0000,
+R_W = 17'b0_0000_1000_0000_0000,
+R1_W = 17'b0_0001_0000_0000_0000,
+R2_W = 17'b0_0010_0000_0000_0000,
+R3_W = 17'b0_0100_0000_0000_0000,
+DR_W = 17'b0_1000_0000_0000_0000,
+AC_W = 17'b1_0000_0000_0000_0000;
 
-    localparam
-    NO_R = 5'd0,
-    INS_MEM_R = 5'd1,
-    DATA_MEM_R = 5'd2,
-    PC_R = 5'd3,
-    IR_R = 5'd4,
-    AR_R = 5'd5,
-    AC_R = 5'd6,
-    X_R = 5'd7,
-    Y_R = 5'd7,
-    Z_R = 5'd8,
-    STXY_R = 5'd9,
-    STYZ_R = 5'd10,
-    STXY_R = 5'd11,
-    R_R = 5'd12,
-    R1_R = 5'd13,
-    R2_R = 5'd14,
-    R3_R = 5'd15,
-    DR_R = 5'd16,
-    AC_R = 5'd17;
+localparam
+NO_R = 5'd0,
+INS_MEM_R = 5'd1,
+DATA_MEM_R = 5'd2,
+PC_R = 5'd3,
+IR_R = 5'd4,
+AR_R = 5'd5,
+AC_R = 5'd6,
+X_R = 5'd7,
+Y_R = 5'd8,
+Z_R = 5'd9,
+STXY_R = 5'd10,
+STYZ_R = 5'd11,
+STXZ_R = 5'd12,
+R_R = 5'd13,
+R1_R = 5'd14,
+R2_R = 5'd15,
+R3_R = 5'd16,
+DR_R = 5'd17;
 
-    localparam
-    NO_I = 6'b00_0000,
-    PC_I = 6'b00_0001,
-    AC_I = 6'd00_0010,
-    STXY_I = 6'd00_0100,
-    STXZ_I = 6'd00_1000,
-    R_I = 6'd01_0000,
-    R3_I = 6'd10_0000;
+localparam
+NO_I = 6'b00_0000,
+PC_I = 6'b00_0001,
+AC_I = 6'd00_0010,
+STXY_I = 6'd00_0100,
+STXZ_I = 6'd00_1000,
+R_I = 6'd01_0000,
+R3_I = 6'd10_0000;
 
-    localparam
-    NO_OP = 3'd0,
-    ADD = 3'd1,
-    MUL = 3'd2,
-    SUB = 3'd3,
-    SFTR = 3'd4,
-    SFTL = 3'd5;
-    ZERO = 3'd6;
+localparam
+NO_OP = 3'd0,
+ADD = 3'd1,
+MUL = 3'd2,
+SUB = 3'd3,
+SFTR = 3'd4,
+SFTL = 3'd5,
+ZERO = 3'd6;
 
-    reg [5:0] CONTROL_COMMAND;
-    reg [5:0] NEXT_COMMAND = FETCH1;
-    always @ (negedge clk) CONTROL_COMMAND <= NEXT_COMMAND;
+reg [5:0] CONTROL_COMMAND;
+reg [5:0] NEXT_COMMAND = FETCH1;
+always @ (negedge clk) CONTROL_COMMAND <= NEXT_COMMAND;
 
-    always @ (CONTROL_COMMAND or Z or instruction)
-	case (CONTROL_COMMAND)
-		FETCH1:
-			begin
+always @ (CONTROL_COMMAND or Z or instruction)
+    case (CONTROL_COMMAND)
+        FETCH1:
+            begin
                 write_enable <= NO_W; // Merge Fetch 1 and Fetch 2 ?
                 read_enable <= INS_MEM_R;
                 increment <= NO_I;
                 alu <= NO_OP;
                 finish <= 0;
                 NEXT_COMMAND <= FETCH2;
-			end
+            end
 
-		FETCH2:
-			begin
+        FETCH2:
+            begin
                 write_enable <= IR_W;
                 read_enable <= INS_MEM_R;
                 increment <= NO_I;
                 alu <= NO_OP;
                 finish <= 0;
                 NEXT_COMMAND <= FETCH2;
-			end
+            end
 
-		FETCH3:
-			begin
+        FETCH3:
+            begin
                 write_enable <= NO_W;
                 read_enable <= NO_R;
                 increment <= PC_I;
                 alu <= NO_OP;
                 finish <= 0;
                 NEXT_COMMAND <= instruction[15:8];
-			end
+            end
     
-		NOP:
-			begin
+        NOP:
+            begin
                 write_enable <= NO_W;
                 read_enable <= NO_R;
                 increment <= NO_I;
                 alu <= NO_OP;
                 finish <= 0;
                 NEXT_COMMAND <= FETCH1;
-			end
+            end
     
-		ADD1:
-			begin
+        ADD1:
+            begin
                 write_enable <= NO_W;
 
                 case (instruction[2:0])
@@ -164,7 +164,7 @@ module control_unit(
                 alu <= ADD;
                 finish <= 0;
                 NEXT_COMMAND <= ADD2;
-			end
+            end
         
         ADD2:
             begin
@@ -177,14 +177,14 @@ module control_unit(
             end
         
         MUL1:
-			begin
+            begin
                 write_enable <= NO_W;
                 read_enable <= R1_R;
                 increment <= NO_I;
                 alu <= ADD;
                 finish <= 0;
                 NEXT_COMMAND <= MUL2;
-			end
+            end
         
         MUL2:
             begin
@@ -197,14 +197,14 @@ module control_unit(
             end
 
         SUB1:
-			begin
+            begin
                 write_enable <= NO_W;
                 read_enable <= R_R;
                 increment <= NO_I;
                 alu <= SUB;
                 finish <= 0;
                 NEXT_COMMAND <= SUB2;
-			end
+            end
         
         SUB2:
             begin
@@ -227,84 +227,84 @@ module control_unit(
             end
 
         LODAC2:
-			begin
+            begin
                 write_enable <= IR_W;
                 read_enable <= INS_MEM_R;
                 increment <= NO_I;
                 alu <= NO_OP;
                 finish <= 0;
                 NEXT_COMMAND <= LODAC3;
-			end
+            end
 
         LODAC3:
-			begin
+            begin
                 write_enable <= AR_W;
                 read_enable <= IR_R;
                 increment <= PC_I;
                 alu <= NO_OP;
                 finish <= 0;
                 NEXT_COMMAND <= LODAC4;
-			end
+            end
         
         LODAC4:
-			begin
+            begin
                 write_enable <= DR_W; //Assuming data will be available by next Positive egdge
                 read_enable <= DATA_MEM_R;
                 increment <= NO_I;
                 alu <= NO_OP;
                 finish <= 0;
                 NEXT_COMMAND <= LODAC5;
-			end
+            end
 
         LODAC5:
-			begin
+            begin
                 write_enable <= AC_W;
                 read_enable <= DR_R;
                 increment <= NO_I;
                 alu <= NO_OP;
                 finish <= 0;
                 NEXT_COMMAND <= FETCH1;
-			end
+            end
         
         LDACAR1:
-			begin
+            begin
                 write_enable <= DR_W;
                 read_enable <= DATA_MEM_R;
                 increment <= NO_I;
                 alu <= NO_OP;
                 finish <= 0;
                 NEXT_COMMAND <= LDACAR2;
-			end
+            end
 
         LDACAR2:
-			begin
+            begin
                 write_enable <= AC_W;
                 read_enable <= DR_R;
                 increment <= NO_I;
                 alu <= NO_OP;
                 finish <= 0;
                 NEXT_COMMAND <= FETCH1;
-			end
+            end
 
         STOAC1:
-			begin
+            begin
                 write_enable <= DR_W;
                 read_enable <= AC_R;
                 increment <= NO_I;
                 alu <= NO_OP;
                 finish <= 0;
                 NEXT_COMMAND <= STOAC2;
-			end
+            end
 
         STOAC2:
-			begin
+            begin
                 write_enable <= DATA_MEM_W;
                 read_enable <= DR_R;
                 increment <= NO_I;
                 alu <= NO_OP;
                 finish <= 0;
                 NEXT_COMMAND <= FETCH1;
-			end
+            end
 
         MVAC:
             begin
@@ -361,7 +361,7 @@ module control_unit(
                 alu <= NO_OP;
                 finish <= 0;
                 NEXT_COMMAND <= FETCH1;
-			end
+            end
         
         MOVREG:
             begin
@@ -418,7 +418,7 @@ module control_unit(
                 alu <= NO_OP;
                 finish <= 0;
                 NEXT_COMMAND <= FETCH1;
-			end
+            end
 
         CLAC1:
             begin
@@ -476,7 +476,7 @@ module control_unit(
                 increment <= NO_I;
                 alu <= NO_OP;
                 finish <= 0;
-                NEXT_COMMAND <= (Z) ? JUMPZN: JUMP;
+                NEXT_COMMAND <= (Z) ? JUMPZN: JUMP1;
             end
         
         JUMPZN:
@@ -496,7 +496,7 @@ module control_unit(
                 increment <= NO_I;
                 alu <= NO_OP;
                 finish <= 0;
-                NEXT_COMMAND <= (Z) ? JUMP: JMNZN;
+                NEXT_COMMAND <= (Z) ? JUMP1: JMNZN;
             end
         
         JMNZN:
@@ -633,10 +633,10 @@ module control_unit(
                 increment <= NO_I;
                 alu <= NO_OP;
                 finish <= 0;
-                NEXT_COMMAND <= (instruction[2:0] == 4 || instruction[2:0] == 6) ? MVAR1: FETCH1;
-			end
+                NEXT_COMMAND <= (instruction[2:0] == 4 || instruction[2:0] == 6) ? MOVAR1: FETCH1;
+            end
 
-        MVAR2
+        MOVAR2:
             begin
                 write_enable <= NO_W;
                 read_enable <= NO_R;
@@ -646,7 +646,7 @@ module control_unit(
                 NEXT_COMMAND <= FETCH1;
             end
         
-        ENDOP
+        ENDOP:
             begin
                 write_enable <= NO_W;
                 read_enable <= NO_R;
@@ -656,4 +656,4 @@ module control_unit(
                 NEXT_COMMAND <= ENDOP;
             end
     endcase
-end module
+endmodule

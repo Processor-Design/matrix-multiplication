b5d01419ea3bbb27f85374e778190c816ad265b2
Kaushalya <kaushalya.s.1998@gamil.com>
2021-07-01 20:38:34 +0530

    Clock divider of processor design

---
+++ clock_divider.v
@@ -0,0 +1,19 @@
+module clock_divider 
+#(parameter divider=1'b1)
+( 
+	input clockin;
+	output clockout;
+);
+
+
+
+reg clockout = 1'b0;
+reg counter = 1'b0;
+
+always @(posedge clockin)
+	begin 
+		counter <= counter + divider;
+		if(counter == 1'b1)
+			clockout <= ~clockout ;
+	end
+endmodule
